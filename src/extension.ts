184e4f5f42d7388c206897b1692df059bffd70d1
Aurélien Pupier <apupier@redhat.com>
2022-01-20 12:02:01 +0100

    Initial debugger
    - debugger jar is checked in source code, it will need to be automated
    when it will be published on maven repositories
    - Specific issue to VS Code: some variables are marked as `invalid
    variable attributes`
    - Not provided a test. It seems to be a hard setup to be ready to
    provide some interesting ones. Want to provide a first iteration
    demoable first.
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,8 @@
 import * as vscode from 'vscode';
+import { CamelDebugAdapterDescriptorFactory } from './CamelDebugAdpaterDescriptorFactory';
 
-export function activate(context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) {
+	vscode.debug.registerDebugAdapterDescriptorFactory('apache.camel', new CamelDebugAdapterDescriptorFactory(context));
+}
 
 export function deactivate() {}
