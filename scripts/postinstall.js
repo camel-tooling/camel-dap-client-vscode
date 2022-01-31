d214b52548dcf3f8b87f4941848265e0abb69aa4
Aurélien Pupier <apupier@redhat.com>
2022-01-31 09:21:15 +0100

    FUSETOOLS2-1426 - retrieve Debug Adapater Server at build time
    it avoids to push the binaries in source code and it allows to pick
    latest version more automatically
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

---
+++ scripts/postinstall.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const dapServerVersion = '0.0.1-SNAPSHOT';
+
+const download = require('mvn-artifact-download').default;
+const fs = require('fs');
+const path = require('path');
+
+download('com.github.camel-tooling:camel-dap-server:' + dapServerVersion, './jars/', 'https://oss.sonatype.org/content/repositories/snapshots/').then((filename)=>{
+	fs.renameSync(filename, path.join('.', 'jars', 'camel-dap-server.jar'));
+});
