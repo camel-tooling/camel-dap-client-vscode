04c410a686feed596e50a83481f798b44814ce26
Aurélien Pupier <apupier@redhat.com>
2022-02-07 14:37:25 +0100

    FUSETOOLS2-1475 - provide metrics on activation/deactivation
    followed readme on
    https://github.com/redhat-developer/vscode-redhat-telemetry
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,17 @@
 import * as vscode from 'vscode';
 import { CamelDebugAdapterDescriptorFactory } from './CamelDebugAdpaterDescriptorFactory';
+import { getRedHatService, TelemetryService } from "@redhat-developer/vscode-redhat-telemetry";
 
-export function activate(context: vscode.ExtensionContext) {
+let telemetryService: TelemetryService;
+
+
+export async function activate(context: vscode.ExtensionContext) {
 	vscode.debug.registerDebugAdapterDescriptorFactory('apache.camel', new CamelDebugAdapterDescriptorFactory(context));
+	const redhatService = await getRedHatService(context);  
+	telemetryService = await redhatService.getTelemetryService();
+	telemetryService.sendStartupEvent();
 }
 
-export function deactivate() {}
+export function deactivate() {
+	telemetryService.sendShutdownEvent();
+}
