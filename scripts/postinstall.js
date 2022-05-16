fc7fb439ed235701f87edc94fc6a9187bb05b8ba
Aurélien Pupier <apupier@redhat.com>
2022-05-16 14:30:16 +0200

    Use tagged 0.3.0 dap server
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const dapServerVersion = '0.3.0-SNAPSHOT';
+const dapServerVersion = '0.3.0';
 
 const download = require('mvn-artifact-download').default;
 const fs = require('fs');
 const path = require('path');
 
-download('com.github.camel-tooling:camel-dap-server:' + dapServerVersion, './jars/', 'https://oss.sonatype.org/content/repositories/snapshots/').then((filename)=>{
+download('com.github.camel-tooling:camel-dap-server:' + dapServerVersion, './jars/', 'https://oss.sonatype.org/content/repositories/releases/').then((filename)=>{
 	fs.renameSync(filename, path.join('.', 'jars', 'camel-dap-server.jar'));
 });
