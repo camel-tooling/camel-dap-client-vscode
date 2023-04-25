b0e24349ebff6d33c5ab7c8b2eeda41821f6c58e
Dominik Jelinek <djelinek@redhat.com>
2023-04-25 22:37:29 +0200

    FUSETOOLS2-2026: Create test to click on Run action button
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- src/ui-test/tests/editor.actions.test.ts
+++ src/ui-test/tests/editor.actions.test.ts
@@ -3,8 +3,10 @@ import * as path from 'path';
 import {
     after,
     before,
-    By,
+    BottomBarPanel,
     EditorView,
+    SideBarView,
+    TerminalView,
     VSBrowser,
     WebElement,
 } from 'vscode-extension-tester';
@@ -16,37 +18,64 @@ describe('Camel file editor test', function () {
     const CAMEL_ROUTE_YAML = 'demo route.camel.yaml';
 
     describe('Camel Actions', function () {
-        this.timeout(30000);
+        this.timeout(180000);
+
+        let editorView: EditorView;
+        let bottomBar: BottomBarPanel;
 
         before('Before setup', async function () {
-            await VSBrowser.instance.openResources(path.resolve('src', 'ui-test', 'resources', CAMEL_ROUTE_YAML));
+            await VSBrowser.instance.openResources(path.resolve('src', 'ui-test', 'resources'));
             await VSBrowser.instance.waitForWorkbench();
+
+            const section = await new SideBarView().getContent().getSection('resources');
+            await section?.openItem(CAMEL_ROUTE_YAML);
+
+            editorView = new EditorView();
+            await section.getDriver().wait(async function () {
+                return (await editorView.getOpenEditorTitles()).find(title => title === CAMEL_ROUTE_YAML);
+            }, 5000);
+
+            bottomBar = new BottomBarPanel();
+            await bottomBar.openTerminalView();
         });
 
         after('After cleanup', async function () {
-            await new EditorView().closeAllEditors();
+            await editorView.closeAllEditors();
         });
 
         it('Debug and Run action is available', async function () {
-            const button = await getAction(CAMEL_RUN_DEBUG_ACTION_LABEL);
+            const button = await editorView.getAction(CAMEL_RUN_DEBUG_ACTION_LABEL);
             expect(button).to.not.undefined;
         });
 
         it('Run action is available', async function () {
-            const button = await getAction(CAMEL_RUN_ACTION_LABEL);
+            const button = await editorView.getAction(CAMEL_RUN_ACTION_LABEL);
             expect(button).to.not.undefined;
+        });
+
+        it(`Can execute '${CAMEL_RUN_ACTION_LABEL}' action`, async function () {
+            const run = await editorView.getAction(CAMEL_RUN_ACTION_LABEL) as WebElement;
+            await run.click();
+
+            await run.getDriver().wait(async function () {
+                console.log('Waiting for JBang dependencies...');
+                await bottomBar.openTerminalView(); // ensure terminal view is opened and focused
+                return (await new TerminalView().getText()).includes('[jbang] Dependencies resolved');
+            }, 60000);
+
+            await run.getDriver().wait(async function () {
+                console.log('Waiting for Camel route to be started...');
+                await bottomBar.openTerminalView(); // ensure terminal view is opened and focused
+                return (await new TerminalView().getText()).includes('(demo route) started');
+            }, 30000);
+
+            const terminal = await bottomBar.openTerminalView(); // ensure terminal view is opened and focused
+            expect(await terminal.getText()).to.contain('Hello Camel');
+
+            await terminal.killTerminal();
+            await bottomBar.toggle(false);
         });
 
     });
 
-    async function getAction(title: string): Promise<WebElement | undefined> {
-        const actions = await new EditorView().findElement(By.className('editor-actions')).findElements(By.className('action-label'));
-        const attribute = VSBrowser.instance.version >= '1.76.0' ? 'aria-label' : 'title';
-        for (const item of actions) {
-            if (await item.getAttribute(attribute) === title) {
-                return item;
-            }
-        }
-        return undefined;
-    }
 });
