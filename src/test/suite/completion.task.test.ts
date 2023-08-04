d789498780fef393085afb8199f19d249334702f
Mikhail Abramov <unsortedhashsets@gmail.com>
2023-08-04 11:49:51 +0200

    Fix floating promises warnings

--- src/test/suite/completion.task.test.ts
+++ src/test/suite/completion.task.test.ts
@@ -3,20 +3,17 @@ import * as vscode from 'vscode';
 import { getDocUri } from './util';
 
 suite('Should do completion in tasks.json', () => {
-	
-	
 	test('Completion for Start application with camel.debug profile', async () => {
 		const docURiTasksJson = getDocUri('.vscode/tasks.json');
 		const expectedCompletion = { label: 'Start Camel application with Maven with camel.debug profile' };
 		await testCompletion(docURiTasksJson, new vscode.Position(3, 7), expectedCompletion);
 	}).timeout(20000);
-	
+
 	test('Completion for Start application with jbang', async () => {
 		const docURiTasksJson = getDocUri('.vscode/tasks.json');
 		const expectedCompletion = { label: 'Run Camel application with JBang with camel-debug' };
 		await testCompletion(docURiTasksJson, new vscode.Position(3, 7), expectedCompletion);
 	}).timeout(20000);
-
 });
 
 async function testCompletion(
@@ -31,11 +28,11 @@ async function testCompletion(
 
 export async function checkExpectedCompletion(docUri: vscode.Uri, position: vscode.Position, expectedCompletion: vscode.CompletionItem) {
 	let hasExpectedCompletion = false;
-	let lastCompletionList : vscode.CompletionList | undefined;
+	let lastCompletionList: vscode.CompletionList | undefined;
 	try {
-		await waitUntil(() => {
+		await waitUntil(async () => {
 			// Executing the command `vscode.executeCompletionItemProvider` to simulate triggering completion
-			(vscode.commands.executeCommand('vscode.executeCompletionItemProvider', docUri, position)).then(value => {
+			await (vscode.commands.executeCommand('vscode.executeCompletionItemProvider', docUri, position)).then(value => {
 				const actualCompletionList = value as vscode.CompletionList;
 				lastCompletionList = actualCompletionList;
 				const completionItemFound = actualCompletionList.items.find(completion => {
@@ -48,7 +45,7 @@ export async function checkExpectedCompletion(docUri: vscode.Uri, position: vsco
 		}, 10000, 500);
 	} catch (err) {
 		let errorMessage = '';
-		if(lastCompletionList) {
+		if (lastCompletionList) {
 			lastCompletionList.items.forEach(completion => {
 				errorMessage += completion.label + '\n';
 			});
