46cd4afa345d29e048574676be538325e362d50f
Aurélien Pupier <apupier@redhat.com>
2023-10-10 17:04:08 +0200

    FUSETOOLS2-2202 - Include Camel DAP SBom
    - Download sbom corresponding to embedded Camel Debug Adapter Server
    - Merge it with the one of the VS Code application
    - Store it on GitHub workflow for now
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -6,6 +6,12 @@ const download = require('mvn-artifact-download').default;
 const fs = require('fs');
 const path = require('path');
 
-download('com.github.camel-tooling:camel-dap-server:' + dapServerVersion, './jars/', 'https://oss.sonatype.org/content/repositories/snapshots/').then((filename)=>{
+const MAVEN_REPO_URL = 'https://oss.sonatype.org/content/repositories/snapshots/';
+
+download('com.github.camel-tooling:camel-dap-server:' + dapServerVersion, './jars/', MAVEN_REPO_URL).then((filename)=>{
 	fs.renameSync(filename, path.join('.', 'jars', 'camel-dap-server.jar'));
+});
+
+download('com.github.camel-tooling:camel-dap-server:json:cyclonedx:' + dapServerVersion, '.', MAVEN_REPO_URL).then((filename)=>{
+	fs.renameSync(filename, path.join('.', 'camel-dap-sbom.json'));
 });
