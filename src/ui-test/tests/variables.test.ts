c991445e6eddb57e55b2cf8fd006adf349929d63
Filip Pospisil <fpospisi@redhat.com>
2024-07-01 15:12:02 +0200

    FUSETOOLS2-2322: Add UI test for Update exchange variable values while debugging

--- src/ui-test/tests/variables.test.ts
+++ src/ui-test/tests/variables.test.ts
@@ -17,6 +17,7 @@
 import { expect } from 'chai';
 import {
     ActivityBar,
+    DebugToolbar,
     EditorView,
     SideBarView,
     TextEditor,
@@ -34,7 +35,7 @@ import {
     isVersionNewer,
     DEBUGGER_ATTACHED_MESSAGE,
 } from '../utils';
-import { RESOURCES_DIR, VARIABLESTEST_JAVA } from '../variables';
+import { RESOURCES_DIR, VARIABLESTEST_YAML } from '../variables';
 
 describe('Display exchange variables values test', function () {
     this.timeout(300000);
@@ -43,32 +44,31 @@ describe('Display exchange variables values test', function () {
     let textEditor: TextEditor;
 
     before(async function () {
+        if (isCamelVersionProductized(process.env.CAMEL_VERSION) || (process.env.CAMEL_VERSION !== undefined && !isVersionNewer("4.4.0", process.env.CAMEL_VERSION))) { // available since Camel 4.4
+            this.skip();
+        }
 
-    if (isCamelVersionProductized(process.env.CAMEL_VERSION) || (process.env.CAMEL_VERSION !== undefined && !isVersionNewer("4.4.0", process.env.CAMEL_VERSION))){ // available since Camel 4.4
-        this.skip();
-    }
+        driver = VSBrowser.instance.driver;
 
-    driver = VSBrowser.instance.driver;
+        await VSBrowser.instance.openResources(RESOURCES_DIR);
+        await (await new ActivityBar().getViewControl('Explorer')).openView();
+        const section = await new SideBarView().getContent().getSection('resources');
 
-    await VSBrowser.instance.openResources(RESOURCES_DIR);
-    await (await new ActivityBar().getViewControl('Explorer')).openView();
-    const section = await new SideBarView().getContent().getSection('resources');
+        await section.openItem(VARIABLESTEST_YAML);
+        const editorView = new EditorView();
+        await driver.wait(async function () {
+            return (await editorView.getOpenEditorTitles()).find(title => title === VARIABLESTEST_YAML);
+        }, 5000);
 
-    await section.openItem(VARIABLESTEST_JAVA);
-    const editorView = new EditorView();
-    await driver.wait(async function () {
-        return (await editorView.getOpenEditorTitles()).find(title => title === VARIABLESTEST_JAVA);
-    }, 5000);
+        await executeCommand(CAMEL_RUN_DEBUG_ACTION_QUICKPICKS_LABEL);
+        await (await new ActivityBar().getViewControl('Run')).openView();
+        await waitUntilTerminalHasText(driver, [DEBUGGER_ATTACHED_MESSAGE], 4000, 120000);
 
-    await executeCommand(CAMEL_RUN_DEBUG_ACTION_QUICKPICKS_LABEL);
-    await (await new ActivityBar().getViewControl('Run')).openView();
-    await waitUntilTerminalHasText(driver, [DEBUGGER_ATTACHED_MESSAGE], 4000, 120000);
-
-    textEditor = new TextEditor();
+        textEditor = new TextEditor();
     });
 
     after(async function () {
-        if (isCamelVersionProductized(process.env.CAMEL_VERSION) || (process.env.CAMEL_VERSION !== undefined && !isVersionNewer("4.4.0", process.env.CAMEL_VERSION))){
+        if (isCamelVersionProductized(process.env.CAMEL_VERSION) || (process.env.CAMEL_VERSION !== undefined && !isVersionNewer("4.4.0", process.env.CAMEL_VERSION))) {
             // do nothing - after is executed even if skip is called in before
         }
         else {
@@ -76,18 +76,34 @@ describe('Display exchange variables values test', function () {
             await (await new ActivityBar().getViewControl('Run and Debug')).closeView();
             await killTerminal();
             await new EditorView().closeAllEditors();
-        }     
+        }
     });
 
     it('Variable is displayed', async function () {
         await driver.wait(async function () {
-            return await textEditor.toggleBreakpoint(9);
+            return await textEditor.toggleBreakpoint(10);
         }, 5000);
-        
+
         const sectionItem = await getDebuggerSectionItem(driver, 'item:', 'Message', 'Variables');
         expect(await sectionItem?.getVariableValue()).to.be.equal("world");
 
         const sectionItem2 = await getDebuggerSectionItem(driver, 'name:', 'Message', 'Variables');
         expect(await sectionItem2?.getVariableValue()).to.be.equal("Camel");
+    });
+
+    const EXCHANGED_VALUE = "exchanged value";
+    const EXCHANGED_LOG = ["Hello exchanged value from Camel"];
+
+    // Tis functionality was added in version 4.4.1. 
+    // https://issues.apache.org/jira/browse/CAMEL-20445
+    it('Update exchange variable values', async function () {
+        if(process.env.CAMEL_VERSION !== undefined && !isVersionNewer("4.4.1", process.env.CAMEL_VERSION)) {
+            this.skip();
+        }
+        const sectionItem = await getDebuggerSectionItem(driver, 'item:', 'Message', 'Variables');
+        await sectionItem?.setVariableValue(EXCHANGED_VALUE);
+        const debugBar = await DebugToolbar.create();
+        await debugBar.continue();
+        await waitUntilTerminalHasText(driver, EXCHANGED_LOG, 4000, 120000);
     });
 });
