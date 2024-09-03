7dfd43e4ff4fb41a8173230d4be6870664e6eb54
Aurélien Pupier <apupier@redhat.com>
2024-09-03 15:48:08 +0200

    prepare next development iteration 1.4.0
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const dapServerVersion = '1.3.0';
+const dapServerVersion = '1.4.0-SNAPSHOT';
 
 const download = require('mvn-artifact-download').default;
 const fs = require('fs');
 const path = require('path');
 
-const MAVEN_REPO_URL = 'https://oss.sonatype.org/content/repositories/releases/';
+const MAVEN_REPO_URL = 'https://oss.sonatype.org/content/repositories/snapshots/';
 
 download('com.github.camel-tooling:camel-dap-server:' + dapServerVersion, './jars/', MAVEN_REPO_URL).then((filename)=>{
 	fs.renameSync(filename, path.join('.', 'jars', 'camel-dap-server.jar'));
