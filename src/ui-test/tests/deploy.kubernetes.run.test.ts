a5e2a6dcdf1130908ab832de28eff684fcb3432e
Dominik Jelinek <djelinek@redhat.com>
2025-01-14 13:52:11 +0100

    test: add downstream UI test for deployment into OpenShift cluster
    which is an extension default setting for deployment feature

--- src/ui-test/tests/deploy.kubernetes.run.test.ts
+++ src/ui-test/tests/deploy.kubernetes.run.test.ts
@@ -14,35 +14,37 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { resolve } from 'node:path';
-import { CAMEL_ROUTE_YAML_WITH_SPACE } from '../variables';
+import { resolve, join } from 'node:path';
 import {
-    ActivityBar,
     after,
+    ArraySetting,
     before,
     BottomBarPanel,
     EditorAction,
     EditorView,
-    SideBarView,
     VSBrowser,
     Workbench,
 } from 'vscode-extension-tester';
 import { killTerminal, waitUntilTerminalHasText } from '../utils';
 import { execSync } from 'child_process';
+import { CAMEL_ROUTE_YAML_WITH_SPACE } from '../variables';
 
+/**
+ * Note: OC login needs to be done before executing this test for deployment into OpenShift
+ */
 describe('Camel standalone file deployment using Camel JBang Kubernetes Run', function () {
-    this.timeout(180_000);
+    this.timeout(600_000); // 10 minutes
 
     let editorView: EditorView;
     let jbangVersion: string;
+    let kubernetesRunParameters: string[];
     const RESOURCES_PATH: string = resolve('src', 'ui-test', 'resources');
 
     before(async function () {
         jbangVersion = await getJBangVersion();
+        kubernetesRunParameters = await getKubernetesRunParameters();
         await VSBrowser.instance.openResources(RESOURCES_PATH);
-        await (await new ActivityBar().getViewControl('Explorer'))?.openView();
-        const section = await new SideBarView().getContent().getSection('resources');
-        await section.openItem(CAMEL_ROUTE_YAML_WITH_SPACE);
+        await VSBrowser.instance.openResources(join(RESOURCES_PATH, CAMEL_ROUTE_YAML_WITH_SPACE));
 
         editorView = new EditorView();
         await editorView.getDriver().wait(async function () {
@@ -54,16 +56,23 @@ describe('Camel standalone file deployment using Camel JBang Kubernetes Run', fu
         await killTerminal();
         await editorView.closeAllEditors();
         // remove deployed integration from a local cluster
-        execSync(`jbang -Dcamel.jbang.version=${jbangVersion} camel@apache/camel kubernetes delete --name=demoroute`, { stdio: 'inherit', cwd: RESOURCES_PATH });
+        if (kubernetesRunParameters[0].includes('openshift')) {
+            // workaround: because of issues with 'camel kubernetes delete --name=<name>' in versions 4.8.1+ and OpenShift we need to cleanup using 'oc'
+            // see https://issues.apache.org/jira/browse/CAMEL-21702
+            execSync(`oc delete deployment demoroute`, { stdio: 'inherit' });
+        } else {
+            // minikube
+            execSync(`jbang -Dcamel.jbang.version=${jbangVersion} camel@apache/camel kubernetes delete --name=demoroute`, { stdio: 'inherit', cwd: RESOURCES_PATH });
+        }
     });
 
-    it('Deploy integration to Kubernetes (Minikube)', async function () {
+    it('Deploy integration to OpenShift or Kubernetes (Minikube)', async function () {
         const action = (await editorView.getAction('Deploy Integration with Apache Camel Kubernetes Run')) as EditorAction;
         await action.click();
 
         // using some additional steps for CAMEL 4.9.0-SNAPSHOT / 4.8.1 version
         // because the '--dev' parameter is not working for a deployment to Kubernetes
-        await waitUntilTerminalHasText(action.getDriver(), ['BUILD SUCCESS'], 3_000, 120_000);
+        await waitUntilTerminalHasText(action.getDriver(), ['BUILD SUCCESS'], 3_000, 600_000); // OpenShift requires higher timeout because it is pulling more dependencies
         await killTerminal();
 
         const terminalView = await new BottomBarPanel().openTerminalView();
@@ -82,6 +91,13 @@ describe('Camel standalone file deployment using Camel JBang Kubernetes Run', fu
         const value = await textField.getValue() as string;
         await new EditorView().closeEditor('Settings');
         return value;
+    }
+
+    async function getKubernetesRunParameters(): Promise<string[]> {
+        const setting = await (await new Workbench().openSettings()).findSetting('Kubernetes Run Parameters', 'Camel', 'Debug Adapter') as ArraySetting;
+        const values = await setting.getValues();
+        await new EditorView().closeEditor('Settings');
+        return values;
     }
 
 });
